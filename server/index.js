323ea0d16345112cc900f8816f202f725b68fdb9
ericJH92 <eric.j.h92@gmail.com>
2022-07-18 19:42:53 -0700

    update-error

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('build','index.html'));
 });
 
+app.get('/bundle.js', (req, res) => {
+    res.sendFile(path.resolve('build', 'bundle.js'));
+})
+
 app.listen(PORT, () => {
     console.log('Server is listening to PORT', PORT);
 });
